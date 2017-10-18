ca8f6fd1ed18cd296aa2889766845cb1963d10b3
terry.nguyen <terryn@aie.edu>
2017-10-18 12:17:32 -0700

    Added Rumba War

---
+++ X-SFWTest/MyGuy.cpp
@@ -0,0 +1,40 @@
+#include "MyGuy.h"
+#include "sfwdraw.h"
+
+myGuy::myGuy(float speed, float rotSpeed, vec2 pos, vec2 dim, float anglez)
+{
+	Speed = speed;
+	turnSpeed = rotSpeed;
+	myTrans.position = pos;
+	myTrans.dimension = dim;
+	myTrans.angle = anglez;
+}
+
+void myGuy::update(const mat3 & t)
+{
+	vec2 Dir = norm((t[1].xy)) * Speed;
+	
+	if (sfw::getKey('W'))
+	{
+		myTrans.position += Dir;
+	}
+	if (sfw::getKey('S'))
+	{
+		myTrans.position -= Dir;
+	}
+	if (sfw::getKey('A'))
+	{
+		myTrans.angle -= turnSpeed;
+	}
+	if (sfw::getKey('D'))
+	{
+		myTrans.angle += turnSpeed;
+	}
+}
+
+void myGuy::draw(const mat3 & t)
+{
+	vec2 pos = t[2].xy;
+
+	sfw::drawCircle(pos.x, pos.y, 20, 12, RED);
+}
--- X-SFWTest/main.cpp
+++ X-SFWTest/main.cpp
@@ -1,7 +1,7 @@
 #include "sfwdraw.h"
 #include "transform.h"
 #include "Player.h"
-
+#include "MyGuy.h"
 #include "mat3.h"
 
 #include <cmath>
@@ -10,28 +10,35 @@ int main()
 {
 	sfw::initContext();
 
-	Transform myTransform;
+
+
+	myGuy me(3, 1, vec2{ 300, 400 }, vec2{ 1,1 }, 0);
+	
+	/*Transform myTransform;
 	myTransform.position = vec2{ 400,300 };
 	myTransform.dimension = vec2{ 2,2 };
 	myTransform.angle = 0;
 	
-
+	
 	Transform myBaby;
 	myBaby.position = vec2{ 100,0 };
 	myBaby.dimension = vec2{ 1,1 };
 	myBaby.angle = 0;
-	myBaby.e_parent = &myTransform;
+	myBaby.e_parent = &myTransform;*/
 	
 	while (sfw::stepContext())
 	{
 	//Rotate your object around clockwise
 		float t = sfw::getTime();
 
-		myTransform.angle += sfw::getDeltaTime() * 90;
-		myTransform.dimension = vec2{ sinf(t) + 2, sinf(t) + 2 };
 
-		DrawMatrix(myTransform.getGlobalTransform(), 40);
-		DrawMatrix(myBaby.getGlobalTransform(), 30);
+		me.update(me.myTrans.getGlobalTransform());
+		me.draw(me.myTrans.getGlobalTransform());
+		//myTransform.angle += sfw::getDeltaTime() * 90;
+		//myTransform.dimension = vec2{ sinf(t) + 2, sinf(t) + 2 };
+
+		DrawMatrix(me.myTrans.getGlobalTransform(), 40);
+		//DrawMatrix(myBaby.getGlobalTransform(), 30);
 	}
 
 	sfw::termContext();
