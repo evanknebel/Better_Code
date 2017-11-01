6765d645291196325300484b7f380fce4260c889
terry.nguyen <terryn@aie.edu>
2017-11-01 10:35:53 -0700

    array of walls example.

--- X-SFWTest/main.cpp
+++ X-SFWTest/main.cpp
@@ -23,33 +23,42 @@ int main()
 	player.transform.position = vec2{ 400,300 };
 	player.collider.box.extents = {.5,.5};
 
-	Wall wall;
-	wall.transform.position = { 400,300 };
-	wall.transform.dimension = {240,240};
-	wall.collider.box.extents = { .5,.5 };
-	wall.sprite.handle = sfw::loadTextureMap("../resources/Giant_Tree.png");
-	
+	Wall walls[2];
+	walls[0].transform.position = { 600,300 };
+	walls[0].transform.dimension = {80,240};
+	walls[0].collider.box.extents = { .5,.5 };
+	walls[0].sprite.handle = sfw::loadTextureMap("../resources/Giant_Tree.png");
+
+	walls[1].transform.position = { 200,300 };
+	walls[1].transform.dimension = { 80,240 };
+	walls[1].collider.box.extents = { .5,.5 };
+	walls[1].sprite.handle = sfw::loadTextureMap("../resources/Giant_Tree.png");
+
 	
 	while (sfw::stepContext())
 	{
 		float dt = sfw::getDeltaTime();
 
+		// update controllers
 		player.controller.poll(player.rigidbody, player.transform);
 
+		// update rigibodies
 		player.rigidbody.integrate(player.transform, dt);
 	
-		player.sprite.draw(player.transform);
-		wall.sprite.draw(wall.transform);
+		// draw stuff
+		player.sprite.draw(player.transform);		
+		for (int i = 0; i < 2; ++i)
+			walls[i].sprite.draw(walls[i].transform);
 
-		doCollision(player, wall);
+		// Collision resolution
+		for (int i = 0; i < 2; ++i)
+			doCollision(player, walls[i]);
 
 
+		// Debug boxes
 		drawAABB(player.collider.getGlobalBox(player.transform), MAGENTA);
-		drawAABB(wall.collider.getGlobalBox(wall.transform), RED);
-
-
-
-		//r = d-2(d.n)n
+		for (int i = 0; i < 2; ++i)
+			drawAABB(walls[i].collider.getGlobalBox(walls[i].transform), RED);
 	}	
 	sfw::termContext();
 }
