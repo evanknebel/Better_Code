fb9e94d806a3c7640652781b584a5395ab926f5c
terry.nguyen <terryn@aie.edu>
2017-10-31 17:05:28 -0700

    components are functional

--- X-SFWTest/main.cpp
+++ X-SFWTest/main.cpp
@@ -2,6 +2,7 @@
 
 #include "sfwdraw.h"
 #include "Player.h"
+#include "DrawShape.h"
 
 
 int main()
@@ -11,11 +12,25 @@ int main()
 
 	Player player;
 
+	player.sprite = sfw::loadTextureMap("../resources/classic_ship.png");
+	player.transform.dimension = vec2{48, 48};
+	player.transform.position = vec2{ 400,300 };
+	player.collider.box.extents = {.5,.5};
 
 	while (sfw::stepContext())
 	{
+		float dt = sfw::getDeltaTime();
 
+		player.controller.poll(player.rigidbody, player.transform);
+
+		player.rigidbody.integrate(player.transform, dt);
+	
+		player.sprite.draw(player.transform);
+
+
+		drawAABB(player.collider.getGlobalBox(player.transform), MAGENTA);
 	}
+
 	sfw::termContext();
 }
 
