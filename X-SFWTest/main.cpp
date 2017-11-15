57eff48b81f18d5bcb0b41c48b8f67d8b4af6d28
s179060 <s179060@students.aie.edu.au>
2017-11-14 17:51:18 -0800

    began platformer

--- X-SFWTest/main.cpp
+++ X-SFWTest/main.cpp
@@ -15,7 +15,7 @@
 
 int main()
 {
-	sfw::initContext();
+	sfw::initContext(1000,1000);
 
 	Player player;
 
@@ -24,16 +24,21 @@ int main()
 	player.transform.position = vec2{ 400,300 };
 	player.collider.box.extents = {.5,.5};
 
-	Wall walls[2];
-	walls[0].transform.position = { 600,300 };
-	walls[0].transform.dimension = {80,240};
+	Wall walls[3];
+	walls[0].transform.position = { 220,300 };
+	walls[0].transform.dimension = {400,10};
 	walls[0].collider.box.extents = { .5,.5 };
-	walls[0].sprite.handle = sfw::loadTextureMap("../resources/Giant_Tree.png");
+	//walls[0].sprite.handle = sfw::loadTextureMap("../resources/Giant_Tree.png");
 
-	walls[1].transform.position = { 200,300 };
-	walls[1].transform.dimension = { 80,240 };
+	walls[1].transform.position = { 750,600 };
+	walls[1].transform.dimension = { 250,10 };
 	walls[1].collider.box.extents = { .5,.5 };
-	walls[1].sprite.handle = walls[0].sprite.handle;
+	//walls[1].sprite.handle = walls[0].sprite.handle;
+
+	walls[2].transform.position = { 40,850 };
+	walls[2].transform.dimension = { 60,10 };
+	walls[2].collider.box.extents = { .5,.5 };
+	//walls[2].sprite.handle = walls[0].sprite.handle;
 
 	Ball ball;
 	ball.transform.position = { 400,300 };
@@ -57,11 +62,11 @@ int main()
 		// draw stuff
 		player.sprite.draw(player.transform);		
 		ball.sprite.draw(ball.transform);
-		for (int i = 0; i < 2; ++i)
+		for (int i = 0; i < 3; ++i)
 			walls[i].sprite.draw(walls[i].transform);
 
 		// Collision resolution
-		for (int i = 0; i < 2; ++i)
+		for (int i = 0; i < 3; ++i)
 		{
 			doCollision(player, walls[i]);
 			doCollision(ball, walls[i]);
@@ -71,7 +76,7 @@ int main()
 
 		// Debug boxes
 		drawAABB(player.collider.getGlobalBox(player.transform), MAGENTA);
-		for (int i = 0; i < 2; ++i)
+		for (int i = 0; i < 3; ++i)
 			drawAABB(walls[i].collider.getGlobalBox(walls[i].transform), RED);
 	}	
 	sfw::termContext();
